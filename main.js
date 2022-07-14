89975931803a81c3a87b2fc94cd14c0190c116d6
The-Estonian <Zaar2213@gmail.com>
2022-07-15 01:15:48 +0300

    created html,css,js files and added preliminary code before logic

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log("JS Connected!");
+
+const display = document.getElementById('display');
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const seven = document.getElementById('seven');
+const eight = document.getElementById('eight');
+const nine = document.getElementById('nine');
+const ten = document.getElementById('ten');
+
+const plus = document.getElementById('plus');
+const minus = document.getElementById('minus');
+const divide = document.getElementById('divide');
+const multiply = document.getElementById('multiply');
+const equals = document.getElementById('equals');
+
+
+
+one.addEventListener('click', (e) => {
+    one.style.background = "black"
+});
